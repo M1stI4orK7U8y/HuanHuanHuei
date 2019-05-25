ae1f330c232571a907ac266488355c6225f31ea9
許修齊 <indexuse@gmail.com>
2019-05-25 22:53:06 +0800

    add dao module

---
+++ src/database/dao/dao.go
@@ -0,0 +1,38 @@
+package dao
+
+import (
+	"sync"
+
+	"github.com/boltdb/bolt"
+)
+
+const (
+	// dbname
+	dbname string = "RECORD.db"
+
+	// table / bucket name
+	recordBucket  string = "RECORD"
+	pendingBucket string = "PENDING"
+)
+
+var instance *Dao
+var once sync.Once
+
+// Dao db struct
+type Dao struct {
+	db *bolt.DB
+}
+
+// Instance get database instance
+func Instance() *Dao {
+	once.Do(func() {
+		instance = &Dao{}
+		instance.db, _ = bolt.Open(dbname, 0600, nil)
+	})
+	return instance
+}
+
+// Close : close db
+func (d *Dao) Close() error {
+	return d.db.Close()
+}
---
+++ src/database/dao/push.go
@@ -0,0 +1,32 @@
+package dao
+
+import (
+	"github.com/boltdb/bolt"
+	"gitlab.com/packtumi9722/huanhuanhuei/src/database/types"
+)
+
+// UpdateRecord add / update a record
+func (d *Dao) UpdateRecord(input types.IRecordType) error {
+	return d.update(input, recordBucket)
+}
+
+// UpdatePending add pending item
+func (d *Dao) UpdatePending(input types.IRecordType) error {
+	return d.update(input, pendingBucket)
+}
+
+func (d *Dao) update(input types.IRecordType, bname string) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bname))
+		if b == nil {
+			newBucket, err := tx.CreateBucket([]byte(bname))
+			if err != nil {
+				return err
+			}
+			b = newBucket
+		}
+		// write to database
+		save, _ := input.Marshal()
+		return b.Put([]byte(input.GetID()), save)
+	})
+}
