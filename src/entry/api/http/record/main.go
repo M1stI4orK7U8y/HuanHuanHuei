256eef1819a03d4e4700e614a6830e0e895d4225
Syu SiouCi <packtumi9722@gmail.com>
2019-05-27 15:38:08 +0800

    implement getRecord api

--- src/entry/api/http/record/main.go
+++ src/entry/api/http/record/main.go
@@ -2,19 +2,35 @@ package record
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
+
+	"gitlab.com/packtumi9722/huanhuanhuei/src/entry/service"
 )
 
 // Routes set route
 func Routes() *chi.Mux {
 	router := chi.NewRouter()
-	router.Get("/{txid}", emptyservice)
-	router.Post("/", emptyservice)
+	router.Get("/{id}", getRecord)
+	router.Post("/", getRecords)
 	return router
 }
 
-func emptyservice(w http.ResponseWriter, r *http.Request) {
+func getRecord(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if len(strings.TrimSpace(id)) == 0 {
+		http.Error(w, http.StatusText(400)+": no id", 400)
+	}
+	ret, err := service.GetRecord(id)
+	if err != nil {
+		http.Error(w, http.StatusText(500)+": "+err.Error(), 500)
+	} else {
+		render.JSON(w, r, ret)
+	}
+}
+
+func getRecords(w http.ResponseWriter, r *http.Request) {
 	render.PlainText(w, r, "test")
 }
