2cd52aea34fc70730d6c9c3d5636f2156e83a861
Syu SiouCi <packtumi9722@gmail.com>
2019-05-28 09:39:02 +0800

    add missing change...

--- src/entry/main.go
+++ src/entry/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi"
 
@@ -20,5 +24,24 @@ func main() {
 	if err := chi.Walk(router, walkFunc); err != nil {
 		log.Panicf("Logging err: %s\n", err.Error()) // panic if there is an error
 	}
-	log.Fatal(http.ListenAndServe(":"+config.Port(), router)) // Note, the port is usually gotten from the environment.
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":"+config.Port(), router)) // Note, the port is usually gotten from the environment.
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		s := <-c
+		log.Printf("get a signal %s", s.String())
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			log.Printf("api service exit")
+			time.Sleep(time.Second)
+			return
+		case syscall.SIGHUP:
+		default:
+			return
+		}
+	}
 }
