2d653b550059e4ba4d0d5aa18f6deee76ae2fd90
Syu SiouCi <packtumi9722@gmail.com>
2019-05-28 09:57:55 +0800

    add test code of POST: /api/v1/record

--- src/entry/main_test.go
+++ src/entry/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 	"testing"
 
 	"gitlab.com/packtumi9722/huanhuanhuei/src/entry/config"
@@ -22,5 +23,17 @@ func TestGetRecord(t *testing.T) {
 		t.Errorf("Failed get record: %s", err.Error())
 	} else {
 		t.Logf("Success get record %s : %v", id, response)
+	}
+}
+
+func TestGetRecords(t *testing.T) {
+	response, err := http.Post("http://127.0.0.1:"+config.Port()+"/api/v1/record",
+		"application/x-www-form-urlencoded",
+		strings.NewReader("ids=123456&ids=456789"))
+
+	if err != nil {
+		t.Errorf("Failed get records: %s", err.Error())
+	} else {
+		t.Logf("Success get records %v", response)
 	}
 }
