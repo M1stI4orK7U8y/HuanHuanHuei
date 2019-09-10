703dd3e41e837a5c5c1865877be92742d0676b43
Syu SiouCi <packtumi9722@gmail.com>
2019-09-10 15:34:00 +0800

    core: fix: if grpc service not available will crash

--- src/huanhuancore/client/grpc/client.go
+++ src/huanhuancore/client/grpc/client.go
@@ -1,6 +1,8 @@
 package grpc
 
 import (
+	"errors"
+
 	agency "gitlab.com/packtumi9722/etcd-agency"
 	"gitlab.com/packtumi9722/huanhuanhuei/src/huanhuancore/config"
 	"google.golang.org/grpc"
@@ -8,6 +10,9 @@ import (
 
 // ConnectDB connect to db
 func ConnectDB() (*grpc.ClientConn, error) {
-	worker := agency.GetServiceWorker(config.DBServiceName())
-	return grpc.Dial(worker.Address, grpc.WithInsecure())
+	if agency.ServiceAvailable(config.DBServiceName()) {
+		worker := agency.GetServiceWorker(config.DBServiceName())
+		return grpc.Dial(worker.Address, grpc.WithInsecure())
+	}
+	return nil, errors.New(config.DBServiceName() + " grpc service not available")
 }
